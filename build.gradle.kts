68d094dfd8584b3aa7d17c7f25ba3678a235227b
Joshua Varghese <joshuadude2715@gmail.com>
2026-01-25 19:42:51 +0530

    FIX: Gradle error

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,22 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
+
 }
+
 buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
     dependencies {
-        classpath "com.google.firebase:firebase-appdistribution-gradle:4.0.0"
+        classpath("com.google.firebase:firebase-appdistribution-gradle:4.0.0")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
 }
